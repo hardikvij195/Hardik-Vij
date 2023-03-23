d6d3224588e367fb1f64ad0bc78127913db25c75
Tamjid-Mostafa <tamjid430@gmail.com>
2023-03-23 20:02:51 +0600

    added classnames module

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import React from 'react'
-
+import Text from '../ui/Text'
+import cn from 'clsx'
+import s from './Hero.module.css'
 const Hero = () => {
   return (
     <div className="h-screen relative">
@@ -12,6 +14,13 @@ const Hero = () => {
           quality={100}
           alt=""
         />
+      </div>
+      <div className='absolute z-10  flex justify-center items-center w-full h-full '>
+        <div className={cn(
+          s.root
+        )}>
+          <Text className={cn(s.hello_text, '')} variant='pageHeading'>Hello, World.</Text>
+        </div>
       </div>
     </div>
   )
